c795777ca520bcc281765644cc8d8052a622ea84
Jason Diehl <gahtzu@gmail.com>
2010-12-28 19:33:17 -0500

    Fresh Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todew::Application.config.secret_token = '5335f2076f816e9251c0a5e2153b169c811ed763ddb82f55257f549fc1acc87356c55cc83e24a0baa9272db4ce7ed2934c31a19b12e8ffd924e075b3c7560abe'
